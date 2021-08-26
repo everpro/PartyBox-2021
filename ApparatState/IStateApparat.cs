8a0caf8a2f3ef4f5a8e66b837abf0466774e8342
Андрей Кримзун <everpro@bk.ru>
2021-08-26 14:46:53 +0300

    Правка 26.08 Добавленн класс Apparat и классы состояния.

---
+++ ApparatState/IStateApparat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyBox_2021.ApparatState
+{
+    interface IStateApparat
+    {
+
+        void startWork ();
+        void stopWork ();
+        void getEvent ();
+        void EnterSetup();
+
+
+    }
+}
